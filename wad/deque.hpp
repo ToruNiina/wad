c8d3831f7cfd83c729dd9d5502c54441cf6d73c4
ToruNiina <niina.toru.68u@gmail.com>
2020-02-20 13:13:36 +0900

    :sparkles: add deque

---
+++ wad/deque.hpp
@@ -0,0 +1,108 @@
+#ifndef WAD_DEQUE_HPP
+#define WAD_DEQUE_HPP
+#include "binary_cast.hpp"
+#include "tag.hpp"
+#include "integer.hpp"
+#include <utility>
+#include <deque>
+
+namespace wad
+{
+template<typename T, typename Alloc, typename Arc>
+bool save(const std::deque<T, Alloc>& v, Arc& arc)
+{
+    const auto savepoint = arc.npos();
+
+    if(v.size() < 16)
+    {
+        const std::uint8_t t = static_cast<std::uint8_t>(tag::fixarray_lower) +
+                               static_cast<std::uint8_t>(v.size());
+        if(!save(static_cast<tag>(t), arc))
+        {
+            arc.seek(savepoint);
+            return false;
+        }
+    }
+    else if(v.size() <= 0xFFFF)
+    {
+        if(!save(tag::array16, arc)) {return false;}
+
+        const std::uint16_t sz = v.size();
+        if(!to_big_endian(sz, arc))
+        {
+            arc.seek(savepoint);
+            return false;
+        }
+    }
+    else if(v.size() <= 0xFFFFFFFF)
+    {
+        if(!save(tag::array32, arc)) {return false;}
+
+        const std::uint32_t sz = v.size();
+        if(!to_big_endian(sz, arc))
+        {
+            arc.seek(savepoint);
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+
+    for(const auto& elem : v)
+    {
+        if(!save(elem, arc))
+        {
+            arc.seek(savepoint);
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T, typename Alloc, typename Arc>
+bool load(std::deque<T, Alloc>& v, Arc& arc)
+{
+    const auto savepoint = arc.npos();
+
+    tag t;
+    if(!load(t, arc)) {return false;}
+
+    const auto byte = static_cast<std::uint8_t>(t);
+    if(        static_cast<std::uint8_t>(tag::fixarray_lower) <= byte &&
+       byte <= static_cast<std::uint8_t>(tag::fixarray_upper))
+    {
+        v.resize(byte - static_cast<std::uint8_t>(tag::fixarray_lower));
+    }
+    else if(t == tag::array16)
+    {
+        std::uint16_t sz = 0;
+        if(!from_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
+        v.resize(sz);
+    }
+    else if(t == tag::array32)
+    {
+        std::uint32_t sz = 0;
+        if(!from_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
+        v.resize(sz);
+    }
+    else
+    {
+        arc.seek(savepoint);
+        return false;
+    }
+
+    for(auto& elem : v)
+    {
+        if(!load(elem, arc))
+        {
+            arc.seek(savepoint);
+            return false;
+        }
+    }
+    return true;
+}
+
+} // wad
+#endif// WAD_DEQUE_HPP
