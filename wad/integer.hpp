8b37e6299725b69ff4b581569a69388a772a024c
ToruNiina <niina.toru.68u@gmail.com>
2020-02-20 12:29:20 +0900

    feat: enable to load integer independent from sign

--- wad/integer.hpp
+++ wad/integer.hpp
@@ -35,16 +35,9 @@ bool save(const std::uint8_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::uint16_t& v, Arc& arc)
 {
-    if(v < 128)
+    if (v <= std::uint16_t(std::numeric_limits<std::uint8_t>::max()))
     {
-        // we don't need to reorder a byte, but for consistency...
-        return to_big_endian(v, arc);
-    }
-    else if (v <= std::uint16_t(std::numeric_limits<std::uint8_t>::max()))
-    {
-        if(!save(tag::uint8, arc))               {return false;}
-        if(!to_big_endian(std::uint8_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
+        return save(std::uint8_t(v), arc);
     }
     else
     {
@@ -57,22 +50,9 @@ bool save(const std::uint16_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::uint32_t& v, Arc& arc)
 {
-    if(v < 128)
+    if (v <= std::uint32_t(std::numeric_limits<std::uint16_t>::max()))
     {
-        // we don't need to reorder a byte, but for consistency...
-        return to_big_endian(v, arc);
-    }
-    else if (v <= std::uint32_t(std::numeric_limits<std::uint8_t>::max()))
-    {
-        if(!save(tag::uint8, arc))               {return false;}
-        if(!to_big_endian(std::uint8_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
-    }
-    else if (v <= std::uint32_t(std::numeric_limits<std::uint16_t>::max()))
-    {
-        if(!save(tag::uint16, arc))               {return false;}
-        if(!to_big_endian(std::uint16_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
+        return save(std::uint16_t(v), arc);
     }
     else
     {
@@ -85,32 +65,13 @@ bool save(const std::uint32_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::uint64_t& v, Arc& arc)
 {
-    if(v < 128)
+    if (v <= std::uint64_t(std::numeric_limits<std::uint32_t>::max()))
     {
-        // we don't need to reorder a byte, but for consistency...
-        return to_big_endian(v, arc);
-    }
-    else if (v <= std::uint64_t(std::numeric_limits<std::uint8_t>::max()))
-    {
-        if(!save(tag::uint8, arc))               {return false;}
-        if(!to_big_endian(std::uint8_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
-    }
-    else if (v <= std::uint64_t(std::numeric_limits<std::uint16_t>::max()))
-    {
-        if(!save(tag::uint16, arc))               {return false;}
-        if(!to_big_endian(std::uint16_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
-    }
-    else if (v <= std::uint64_t(std::numeric_limits<std::uint32_t>::max()))
-    {
-        if(!save(tag::uint32, arc))               {return false;}
-        if(!to_big_endian(std::uint32_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
+        return save(std::uint32_t(v), arc);
     }
     else
     {
-        if(!save(tag::uint32, arc)) {return false;}
+        if(!save(tag::uint64, arc)) {return false;}
         if(!to_big_endian(v, arc))  {arc.retrace(1); return false;}
         return true;
     }
@@ -134,6 +95,14 @@ bool load(std::uint8_t& v, Arc& arc)
     else if(t == tag::uint8)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::int8)
+    {
+        std::int8_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(2); return false;}
+        v = buf; // if it's positive, then it's okay.
         return true;
     }
     else
@@ -165,6 +134,22 @@ bool load(std::uint16_t& v, Arc& arc)
     else if(t == tag::uint16)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::int8)
+    {
+        std::int8_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(2); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int16)
+    {
+        std::int16_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(3); return false;}
+        v = buf; // if it's positive, then it's okay.
         return true;
     }
     else
@@ -203,6 +188,30 @@ bool load(std::uint32_t& v, Arc& arc)
     else if(t == tag::uint32)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::int8)
+    {
+        std::int8_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(2); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int16)
+    {
+        std::int16_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(3); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int32)
+    {
+        std::int32_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(5); return false;}
+        v = buf; // if it's positive, then it's okay.
         return true;
     }
     else
@@ -250,6 +259,38 @@ bool load(std::uint64_t& v, Arc& arc)
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
         return true;
     }
+    else if(t == tag::int8)
+    {
+        std::int8_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(2); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int16)
+    {
+        std::int16_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(3); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int32)
+    {
+        std::int32_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(5); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
+    else if(t == tag::int64)
+    {
+        std::int32_t buf;
+        if(!from_big_endian(buf, arc)) {arc.retrace(1); return false;}
+        if(buf < 0)                    {arc.retrace(9); return false;}
+        v = buf; // if it's positive, then it's okay.
+        return true;
+    }
     else
     {
         arc.retrace(1);
@@ -273,7 +314,7 @@ bool save(const std::int8_t& v, Arc& arc)
     }
     else
     {
-        if(!save(tag::int8, arc)) {return false;};
+        if(!save(tag::int8, arc))  {return false;}
         if(!to_big_endian(v, arc)) {arc.retrace(1); return false;}
         return true;
     }
@@ -282,17 +323,13 @@ bool save(const std::int8_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::int16_t& v, Arc& arc)
 {
-    if(-32 <= v && v < 128)
+    if(0 <= v)
     {
-        // we don't need to reorder a byte, but for consistency...
-        return to_big_endian(std::int8_t(v), arc);
+        return save(std::uint16_t(v));
     }
-    else if (std::int16_t(std::numeric_limits<std::int8_t>::min) <= v &&
-        v <= std::int16_t(std::numeric_limits<std::int8_t>::max))
+    else if (std::int16_t(std::numeric_limits<std::int8_t>::min) <= v)
     {
-        if(!save(tag::int8, arc))               {return false;}
-        if(!to_big_endian(std::int8_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
+        return save(std::int8_t(v), arc);
     }
     else
     {
@@ -305,24 +342,13 @@ bool save(const std::int16_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::int32_t& v, Arc& arc)
 {
-    if(-32 <= v && v < 128)
+    if(0 <= v)
     {
-        // we don't need to reorder a byte, but for consistency...
-        return to_big_endian(std::int8_t(v), arc);
+        return save(std::uint32_t(v));
     }
-    else if (std::int32_t(std::numeric_limits<std::int8_t>::min) <= v &&
-        v <= std::int32_t(std::numeric_limits<std::int8_t>::max))
+    else if (std::int32_t(std::numeric_limits<std::int16_t>::min) <= v)
     {
-        if(!save(tag::int8, arc))               {return false;}
-        if(!to_big_endian(std::int8_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
-    }
-    else if (std::int32_t(std::numeric_limits<std::int16_t>::min) <= v &&
-        v <= std::int32_t(std::numeric_limits<std::int16_t>::max))
-    {
-        if(!save(tag::int16, arc))               {return false;}
-        if(!to_big_endian(std::int16_t(v), arc)) {arc.retrace(1); return false;}
-        return true;
+        return save(std::int16_t(v), arc);
     }
     else
     {
@@ -335,30 +361,13 @@ bool save(const std::int32_t& v, Arc& arc)
 template<typename Arc>
 bool save(const std::int64_t& v, Arc& arc)
 {
-    if(-32 <= v && v < 128)
+    if(0 <= v)
     {
-        return to_big_endian(std::int8_t(v), arc);
+        return save(std::uint64_t(v));
     }
-    else if (std::int64_t(std::numeric_limits<std::int8_t>::min) <= v &&
-        v <= std::int64_t(std::numeric_limits<std::int8_t>::max))
+    else if (std::int64_t(std::numeric_limits<std::int32_t>::min) <= v)
     {
-        if(!save(tag::int8, arc))              {return false;}
-        if(!to_big_endian(std::int8_t(v), arc)){arc.retrace(1); return false;}
-        return true;
-    }
-    else if (std::int64_t(std::numeric_limits<std::int16_t>::min) <= v &&
-        v <= std::int64_t(std::numeric_limits<std::int16_t>::max))
-    {
-        if(!save(tag::int16, arc))              {return false;}
-        if(!to_big_endian(std::int16_t(v), arc)){arc.retrace(1); return false;}
-        return true;
-    }
-    else if (std::int64_t(std::numeric_limits<std::int32_t>::min) <= v &&
-        v <= std::int64_t(std::numeric_limits<std::int32_t>::max))
-    {
-        if(!save(tag::int32, arc))              {return false;}
-        if(!to_big_endian(std::int32_t(v), arc)){arc.retrace(1); return false;}
-        return true;
+        return save(std::int32_t(v), arc);
     }
     else
     {
@@ -379,16 +388,24 @@ bool load(std::int8_t& v, Arc& arc)
 
     const auto byte = static_cast<std::uint8_t>(t);
     if(byte <= static_cast<std::uint8_t>(tag::positive_fixint_upper) ||
-               static_cast<std::uint8_t>(tag::negative_fixint_upper) <= byte)
+               static_cast<std::uint8_t>(tag::negative_fixint_lower) <= byte)
     {
         // cstdints are guaranteed to be represented as 2's complement.
         std::copy_n(reinterpret_cast<const char*>(std::addressof(byte)), 1,
-                    reinterpret_cast<char>(std::addressof(v)));
+                    reinterpret_cast<char*>(std::addressof(v)));
         return true;
     }
     else if(t == tag::int8)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::uint8)
+    {
+        std::uint8_t u8;
+        if(!from_big_endian(u8, arc)) {arc.retrace(1); return false;}
+        if(127u < u8)                 {arc.retrace(2); return false;}
+        v = u8;
         return true;
     }
     else
@@ -425,6 +442,25 @@ bool load(std::int16_t& v, Arc& arc)
     else if(t == tag::int16)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::uint8)
+    {
+        std::uint8_t u8;
+        if(!from_big_endian(u8, arc)) {arc.retrace(1); return false;}
+        v = u8;
+        return true;
+    }
+    else if(t == tag::uint16)
+    {
+        std::uint16_t u16;
+        if(!from_big_endian(u16, arc)) {arc.retrace(1); return false;}
+        if(std::numeric_limits<std::int16_t>::max() < u16)
+        {
+            arc.retrace(2);
+            return false;
+        }
+        v = u16;
         return true;
     }
     else
@@ -468,6 +504,32 @@ bool load(std::int32_t& v, Arc& arc)
     else if(t == tag::int32)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::uint8)
+    {
+        std::uint8_t u8;
+        if(!from_big_endian(u8, arc)) {arc.retrace(1); return false;}
+        v = u8;
+        return true;
+    }
+    else if(t == tag::uint16)
+    {
+        std::uint16_t u16;
+        if(!from_big_endian(u16, arc)) {arc.retrace(1); return false;}
+        v = u16;
+        return true;
+    }
+    else if(t == tag::uint32)
+    {
+        std::uint32_t u32;
+        if(!from_big_endian(u32, arc)) {arc.retrace(1); return false;}
+        if(std::numeric_limits<std::int32_t>::max() < u32)
+        {
+            arc.retrace(5);
+            return false;
+        }
+        v = u32;
         return true;
     }
     else
@@ -518,6 +580,39 @@ bool load(std::int64_t& v, Arc& arc)
     else if(t == tag::int64)
     {
         if(!from_big_endian(v, arc)) {arc.retrace(1); return false;}
+        return true;
+    }
+    else if(t == tag::uint8)
+    {
+        std::uint8_t u8;
+        if(!from_big_endian(u8, arc)) {arc.retrace(1); return false;}
+        v = u8;
+        return true;
+    }
+    else if(t == tag::uint16)
+    {
+        std::uint16_t u16;
+        if(!from_big_endian(u16, arc)) {arc.retrace(1); return false;}
+        v = u16;
+        return true;
+    }
+    else if(t == tag::uint32)
+    {
+        std::uint32_t u32;
+        if(!from_big_endian(u32, arc)) {arc.retrace(1); return false;}
+        v = u32;
+        return true;
+    }
+    else if(t == tag::uint64)
+    {
+        std::uint64_t u64;
+        if(!from_big_endian(u64, arc)) {arc.retrace(1); return false;}
+        if(std::numeric_limits<std::int64_t>::max() < u64)
+        {
+            arc.retrace(9);
+            return false;
+        }
+        v = u64;
         return true;
     }
     else
