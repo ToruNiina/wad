0f8f67ecdb9b45b1b6990ec96596b9431bc6029a
ToruNiina <niina.toru.68u@gmail.com>
2020-02-20 13:08:01 +0900

    :bug: fix byte-length of size specifier
    normal integers should be shortened, but size specifier should not.

--- wad/string.hpp
+++ wad/string.hpp
@@ -12,40 +12,36 @@ namespace wad
 template<typename Traits, typename Alloc, typename Arc>
 bool save(const std::basic_string<char, Traits, Alloc>& v, Arc& arc)
 {
-    std::size_t written = 0;
+    const auto savepoint = arc.npos();
     if(v.size() < 32)
     {
         const std::uint8_t t = static_cast<std::uint8_t>(tag::fixstr_lower) +
                                static_cast<std::uint8_t>(v.size());
         if(!save(static_cast<tag>(t), arc)) {return false;}
-        written = 1;
     }
     else if(v.size() <= 0xFF)
     {
         if(!save(tag::str8, arc)) {return false;}
         const std::uint8_t sz = v.size();
-        save(sz, arc);
-        written = 2;
+        if(!to_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
     }
     else if(v.size() <= 0xFFFF)
     {
         if(!save(tag::str16, arc)) {return false;}
         const std::uint16_t sz = v.size();
-        save(sz, arc);
-        written = 3;
+        if(!to_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
     }
     else if(v.size() <= 0xFFFFFFFF)
     {
         if(!save(tag::str32, arc)) {return false;}
         const std::uint32_t sz = v.size();
-        save(sz, arc);
-        written = 5;
+        if(!to_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
     }
     else
     {
         return false;
     }
-    if(!arc.is_writable(v.size())) {arc.retrace(written); return false;}
+    if(!arc.is_writable(v.size())) {arc.seek(savepoint); return false;}
 
     std::copy(v.begin(), v.end(), arc.sink());
     arc.advance(v.size());
@@ -55,45 +51,41 @@ bool save(const std::basic_string<char, Traits, Alloc>& v, Arc& arc)
 template<typename Traits, typename Alloc, typename Arc>
 bool load(std::basic_string<char, Traits, Alloc>& v, Arc& arc)
 {
+    const auto savepoint = arc.npos();
+
     tag t;
     if(!load(t, arc)) {return false;}
-
-    std::size_t read = 0;
 
     const auto byte = static_cast<std::uint8_t>(t);
     if(        static_cast<std::uint8_t>(tag::fixstr_lower) <= byte &&
        byte <= static_cast<std::uint8_t>(tag::fixstr_upper))
     {
         v.resize(byte - static_cast<std::uint8_t>(tag::fixstr_lower));
-        read = 1;
     }
     else if(t == tag::str8)
     {
         std::uint8_t sz = 0;
-        if(!load(sz, arc)) {arc.retrace(1); return false;}
+        if(!from_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
         v.resize(sz);
-        read = 2;
     }
     else if(t == tag::str16)
     {
         std::uint16_t sz = 0;
-        if(!load(sz, arc)) {arc.retrace(1); return false;}
+        if(!from_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
         v.resize(sz);
-        read = 3;
     }
     else if(t == tag::str32)
     {
         std::uint32_t sz = 0;
-        if(!load(sz, arc)) {arc.retrace(1); return false;}
+        if(!from_big_endian(sz, arc)) {arc.seek(savepoint); return false;}
         v.resize(sz);
-        read = 5;
     }
     else
     {
-        arc.retrace(1);
+        arc.seek(savepoint);
         return false;
     }
-    if(!arc.is_readable(v.size())) {arc.retrace(read); return false;}
+    if(!arc.is_readable(v.size())) {arc.seek(savepoint); return false;}
 
     std::copy_n(arc.src(), v.size(), v.begin());
     arc.advance(v.size());
