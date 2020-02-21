13bf1e0f2ab79053f229662faa6e35cdf362c192
ToruNiina <niina.toru.68u@gmail.com>
2020-02-21 11:32:17 +0900

    :recycle: move test archives to test/utility

--- tests/test_floating.cpp
+++ tests/test_floating.cpp
@@ -1,77 +1,16 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "utility.hpp"
 #include "wad/floating.hpp"
-
-class test_write_archive
-{
-  public:
-
-    std::back_insert_iterator<std::vector<std::uint8_t>>
-    sink() noexcept {return std::back_inserter(buffer_);}
-
-    bool is_writable(const std::size_t) const noexcept {return true;}
-
-    void advance(const std::size_t) noexcept {return;}
-
-    void retrace(const std::size_t n) noexcept
-    {
-        for(std::size_t i=0; i<n; ++i) {buffer_.pop_back();}
-        return;
-    }
-
-    std::size_t npos() const noexcept {return buffer_.size();}
-
-    void seek(const std::size_t n) noexcept
-    {
-        buffer_.erase(buffer_.begin() + n, buffer_.end());
-        return ;
-    }
-
-    std::vector<std::uint8_t> const& buffer() const noexcept {return buffer_;}
-
-  private:
-    std::vector<std::uint8_t> buffer_;
-};
-
-struct test_read_archive
-{
-    test_read_archive(const test_write_archive& wa)
-        : buffer_(wa.buffer()), iter_(buffer_.begin())
-    {}
-
-    std::vector<std::uint8_t>::const_iterator
-    src() const noexcept {return iter_;}
-
-    bool is_readable(const std::size_t len) const noexcept
-    {
-        const std::size_t residual = std::distance(iter_, buffer_.end());
-        return len <= residual;
-    }
-
-    void advance(const std::size_t len) noexcept {iter_ += len; return ;}
-    void retrace(const std::size_t len) noexcept {iter_ -= len; return ;}
-
-    std::vector<std::uint8_t>::const_iterator npos() const noexcept {return iter_;}
-
-    void seek(const std::vector<std::uint8_t>::const_iterator it) noexcept
-    {
-        iter_ = it;
-        return ;
-    }
-
-  private:
-    std::vector<std::uint8_t> buffer_;
-    std::vector<std::uint8_t>::const_iterator iter_;
-};
 
 template<typename T>
 T save_load(const T& t)
 {
-    test_write_archive warc;
+    wad::test_write_archive warc;
     wad::save(t, warc);
 
     T retval;
-    test_read_archive rarc(warc);
+    wad::test_read_archive rarc(warc);
     wad::load(retval, rarc);
 
     return retval;
---
+++ tests/utility.hpp
@@ -0,0 +1,71 @@
+#ifndef WAD_TEST_UTILITY_HPP
+#define WAD_TEST_UTILITY_HPP
+#include <vector>
+#include <cstdint>
+
+namespace wad
+{
+class test_write_archive
+{
+  public:
+
+    std::back_insert_iterator<std::vector<std::uint8_t>>
+    sink() noexcept {return std::back_inserter(buffer_);}
+
+    bool is_writable(const std::size_t) const noexcept {return true;}
+
+    void advance(const std::size_t) noexcept {return;}
+
+    void retrace(const std::size_t n) noexcept
+    {
+        for(std::size_t i=0; i<n; ++i) {buffer_.pop_back();}
+        return;
+    }
+
+    std::size_t npos() const noexcept {return buffer_.size();}
+
+    void seek(const std::size_t n) noexcept
+    {
+        buffer_.erase(buffer_.begin() + n, buffer_.end());
+        return ;
+    }
+
+    std::vector<std::uint8_t> const& buffer() const noexcept {return buffer_;}
+
+  private:
+    std::vector<std::uint8_t> buffer_;
+};
+
+struct test_read_archive
+{
+    test_read_archive(const test_write_archive& wa)
+        : buffer_(wa.buffer()), iter_(buffer_.begin())
+    {}
+
+    std::vector<std::uint8_t>::const_iterator
+    src() const noexcept {return iter_;}
+
+    bool is_readable(const std::size_t len) const noexcept
+    {
+        const std::size_t residual = std::distance(iter_, buffer_.end());
+        return len <= residual;
+    }
+
+    void advance(const std::size_t len) noexcept {iter_ += len; return ;}
+    void retrace(const std::size_t len) noexcept {iter_ -= len; return ;}
+
+    std::vector<std::uint8_t>::const_iterator npos() const noexcept {return iter_;}
+
+    void seek(const std::vector<std::uint8_t>::const_iterator it) noexcept
+    {
+        iter_ = it;
+        return ;
+    }
+
+  private:
+    std::vector<std::uint8_t> buffer_;
+    std::vector<std::uint8_t>::const_iterator iter_;
+};
+
+} // wad
+#endif// WAD_TEST_UTILITY_HPP
