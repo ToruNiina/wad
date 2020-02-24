d5fe41b40436f045e3f2ce09b05872305736c7e1
ToruNiina <niina.toru.68u@gmail.com>
2020-02-24 10:10:00 +0900

    :white_check_mark: add test_array

---
+++ tests/test_array.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "wad/array.hpp"
+#include "wad/integer.hpp"
+
+#include "utility.hpp"
+
+TEST_CASE( "std::array save/load", "[std::array]" )
+{
+    const std::array<int, 5>     upto_16{{1,2,3,4,5}};
+
+    std::array<int, 20>    upto_u16;
+    std::array<int, 70000> upto_u32;
+    for(int i=0; i<20;    ++i) {upto_u16[i] = i;}
+    for(int i=0; i<70000; ++i) {upto_u32[i] = i;}
+
+    REQUIRE(wad::save_load(upto_16)  == upto_16);
+    REQUIRE(wad::save_load(upto_u16) == upto_u16);
+    REQUIRE(wad::save_load(upto_u32) == upto_u32);
+}
