9c9f7e41c4c55a41d05ab22374aaf9e177dfb9d2
ToruNiina <niina.toru.68u@gmail.com>
2020-02-26 00:26:56 +0900

    :truck: move metafunctions to type_traits

--- wad/extensions.hpp
+++ wad/extensions.hpp
@@ -3,29 +3,11 @@
 #include "archive.hpp"
 #include "type_traits.hpp"
 
-// These template functions will be activated only when a type T has memfun
-// bool save(Arc&) and bool load(Arc&).
-
 namespace wad
 {
-namespace detail
-{
-// this also finds template member functions.
-template<typename T, bool (T::*ptr)(wad::write_archive&) const = &T::save>
-std::true_type  has_save_method_impl(T*);
-template<typename T>
-std::false_type has_save_method_impl(...);
 
-template<typename T, bool (T::*ptr)(wad::read_archive&) = &T::load>
-std::true_type  has_load_method_impl(T*);
-template<typename T>
-std::false_type has_load_method_impl(...);
-} // detail
-
-template<typename T>
-struct has_save_method : decltype(detail::has_save_method_impl<T>(nullptr)) {};
-template<typename T>
-struct has_load_method : decltype(detail::has_load_method_impl<T>(nullptr)) {};
+// ---------------------------------------------------------------------------
+// support v.save(arc) and v.load(arc);
 
 template<typename T, typename Arc>
 typename std::enable_if<has_save_method<T>::value, bool>::type
@@ -42,24 +24,6 @@ load(Arc& arc, T& v)
 
 // ---------------------------------------------------------------------------
 // support v.archive(arc);
-
-namespace detail
-{
-template<typename T, bool (T::*ptr)(wad::write_archive&) = &T::archive>
-std::true_type  has_write_archive_method_impl(T*);
-template<typename T>
-std::false_type has_write_archive_method_impl(...);
-
-template<typename T, bool (T::*ptr)(wad::read_archive&) = &T::archive>
-std::true_type  has_read_archive_method_impl(T*);
-template<typename T>
-std::false_type has_read_archive_method_impl(...);
-} // detail
-
-template<typename T>
-struct has_write_archive_method: decltype(detail::has_write_archive_method_impl<T>(nullptr)) {};
-template<typename T>
-struct has_read_archive_method : decltype(detail::has_write_archive_method_impl<T>(nullptr)) {};
 
 template<typename T, typename Arc>
 typename std::enable_if<detail::conjunction<has_write_archive_method<T>,
--- wad/type_traits.hpp
+++ wad/type_traits.hpp
@@ -4,6 +4,10 @@
 
 namespace wad
 {
+
+// ============================================================================
+// std::-like meta functions
+
 namespace detail
 {
 
@@ -43,7 +47,53 @@ struct disjunction<T, Ts...> :
 template<typename T>
 struct negation : std::integral_constant<bool, !static_cast<bool>(T::value)>{};
 
-
 } // detail
+
+// ============================================================================
+// has_xxx
+
+// ----------------------------------------------------------------------------
+// has_save_method, has_load_method
+
+namespace detail
+{
+// this also finds template member functions.
+template<typename T, bool (T::*ptr)(wad::write_archive&) const = &T::save>
+std::true_type  has_save_method_impl(T*);
+template<typename T>
+std::false_type has_save_method_impl(...);
+
+template<typename T, bool (T::*ptr)(wad::read_archive&) = &T::load>
+std::true_type  has_load_method_impl(T*);
+template<typename T>
+std::false_type has_load_method_impl(...);
+} // detail
+
+template<typename T>
+struct has_save_method : decltype(detail::has_save_method_impl<T>(nullptr)) {};
+template<typename T>
+struct has_load_method : decltype(detail::has_load_method_impl<T>(nullptr)) {};
+
+// ----------------------------------------------------------------------------
+// has_write_archive_method, has_read_archive_method
+
+namespace detail
+{
+template<typename T, bool (T::*ptr)(wad::write_archive&) = &T::archive>
+std::true_type  has_write_archive_method_impl(T*);
+template<typename T>
+std::false_type has_write_archive_method_impl(...);
+
+template<typename T, bool (T::*ptr)(wad::read_archive&) = &T::archive>
+std::true_type  has_read_archive_method_impl(T*);
+template<typename T>
+std::false_type has_read_archive_method_impl(...);
+} // detail
+
+template<typename T>
+struct has_write_archive_method: decltype(detail::has_write_archive_method_impl<T>(nullptr)) {};
+template<typename T>
+struct has_read_archive_method : decltype(detail::has_write_archive_method_impl<T>(nullptr)) {};
+
 } // wad
 #endif// WAD_TYPE_TRAITS_HPP
