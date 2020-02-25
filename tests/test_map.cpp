cbe131cdd6b1f60571dca2f8f2cd471b65fd2399
ToruNiina <niina.toru.68u@gmail.com>
2020-02-25 23:59:59 +0900

    :white_check_mark: add test case to maps

--- tests/test_map.cpp
+++ tests/test_map.cpp
@@ -21,3 +21,19 @@ TEST_CASE( "std::map<std::string, int> save/load", "[std::map<std::string, int>]
     REQUIRE(wad::save_load(upto_u16) == upto_u16);
     REQUIRE(wad::save_load(upto_u32) == upto_u32);
 }
+
+TEST_CASE( "std::map<int, std::string> save/load", "[std::map<int, std::string>]" )
+{
+    const std::map<int, std::string> upto_16{
+        {1, "foo"}, {2, "bar"}, {3, "baz"},
+        {4, "qux"}, {5, "quux"}
+    };
+    std::map<int, std::string> upto_u16;
+    std::map<int, std::string> upto_u32;
+    for(int i=0; i<20;    ++i) {upto_u16[i] = std::to_string(i);}
+    for(int i=0; i<70000; ++i) {upto_u32[i] = std::to_string(i);}
+
+    REQUIRE(wad::save_load(upto_16)  == upto_16);
+    REQUIRE(wad::save_load(upto_u16) == upto_u16);
+    REQUIRE(wad::save_load(upto_u32) == upto_u32);
+}
--- tests/test_unordered_map.cpp
+++ tests/test_unordered_map.cpp
@@ -21,3 +21,19 @@ TEST_CASE( "std::unordered_map<std::string, int> save/load", "[std::unordered_ma
     REQUIRE(wad::save_load(upto_u16) == upto_u16);
     REQUIRE(wad::save_load(upto_u32) == upto_u32);
 }
+
+TEST_CASE( "std::unordered_map<int, std::string> save/load", "[std::unordered_map<int, std::string>]" )
+{
+    const std::unordered_map<int, std::string> upto_16{
+        {1, "foo"}, {2, "bar"}, {3, "baz"},
+        {4, "qux"}, {5, "quux"}
+    };
+    std::unordered_map<int, std::string> upto_u16;
+    std::unordered_map<int, std::string> upto_u32;
+    for(int i=0; i<20;    ++i) {upto_u16[i] = std::to_string(i);}
+    for(int i=0; i<70000; ++i) {upto_u32[i] = std::to_string(i);}
+
+    REQUIRE(wad::save_load(upto_16)  == upto_16);
+    REQUIRE(wad::save_load(upto_u16) == upto_u16);
+    REQUIRE(wad::save_load(upto_u32) == upto_u32);
+}
