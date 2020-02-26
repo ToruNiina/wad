ba75ce9c39627911a0d95cdc4b52f92c3266d7fd
ToruNiina <niina.toru.68u@gmail.com>
2020-02-27 02:12:40 +0900

    :pencil2: fix incorrect commas in SFINAE condition

--- wad/archive.hpp
+++ wad/archive.hpp
@@ -10,8 +10,8 @@ namespace wad
 template<typename Arc, typename T>
 // arc.sink() is valid, arc.src() is not valid, & arg.archive(arc) isn't defined
 typename std::enable_if<detail::conjunction<
-    has_sink_method<Arc>, detail::negation<has_src_method<Arc>>
-    detail::negation<has_write_archive_method<T>>,
+    has_sink_method<Arc>, detail::negation<has_src_method<Arc>>,
+    detail::negation<has_write_archive_method<T>>
     >::value, bool>::type
 archive(Arc& arc, T& arg)
 {
@@ -20,8 +20,8 @@ archive(Arc& arc, T& arg)
 template<typename Arc, typename T>
 // arc.src() is valid, arc.sink() is not valid, & arg.archive(arc) isn't defined
 typename std::enable_if<detail::conjunction<
-    has_src_method<Arc>, detail::negation<has_sink_method<Arc>>
-    detail::negation<has_read_archive_method<T>>,
+    has_src_method<Arc>, detail::negation<has_sink_method<Arc>>,
+    detail::negation<has_read_archive_method<T>>
     >::value, bool>::type
 archive(Arc& arc, T& arg)
 {
